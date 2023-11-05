cb4f13f451bdb3599d79565228481c69fa85bcb4
PhcdY <phucb2411@gmail.com>
2023-11-05 17:22:10 +0700

    zz

--- Testing.py
+++ Testing.py
@@ -4,3 +4,10 @@ for i in range(0, 5):
     print("what's up with your code man? It's complicated as hell")
 print("Hieu beo i")
 print("mmb")
+<<<<<<< Updated upstream
+=======
+for i in range(1,199):
+    print("mmb")
+
+    
+>>>>>>> Stashed changes
